2fe7e4317aa2d96a4c37394b3443cc1e7ab687b5
JPCarreiro13 <joao.p.carreiro@gmail.com>
2022-05-24 17:05:06 +0100

    new-menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -90,3 +90,17 @@ function toggleAccordion() {
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+
+// MENU
+
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
